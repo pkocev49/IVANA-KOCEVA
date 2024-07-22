b3786989c8abaee081af04b6f26e9a56aa80645c
Pavel Kocev <pkocev49@gmail.com>
2024-07-22 10:52:17 +0200

    update

--- portfolio-ivana/src/components/pages/Home.vue
+++ portfolio-ivana/src/components/pages/Home.vue
@@ -1,22 +1,24 @@
 <template>
-  <section id="home" class="top">
+  <section id="home">
     <div class="firstContainer">
       <div>
         <img class="firstImage" src="../../assets/Images/vector.svg" alt="Hero Image" />
       </div>
       <div class="seccondContainer">
-        <h2 class="title">{{ title }}</h2>
-        <h3 class="subHeading">{{ subHeading }}</h3>
+        <h2 id="title" class="title"></h2>
+        <h3 id="subHeading" class="subHeading"></h3>
       </div>
     </div>
-    <div class="triangle"></div>
+    <!-- <div class="triangle"></div> -->
   </section>
-  <div class="bottom"></div>
+  <!-- <div class="bottom"></div> -->
   <WhatYouGet />
 </template>
 
 <script>
+import Typewriter from 'typewriter-effect/dist/core'
 import WhatYouGet from '../WhatYouGet.vue'
+
 export default {
   components: {
     WhatYouGet
@@ -26,6 +28,21 @@ export default {
       title: 'Придружи се во тимот на успешните',
       subHeading: 'И заработувај многу'
     }
+  },
+  mounted() {
+    new Typewriter('#title', {
+      strings: [this.title],
+      autoStart: true,
+      delay: 75,
+      loop: true
+    })
+
+    new Typewriter('#subHeading', {
+      strings: [this.subHeading],
+      autoStart: true,
+      delay: 75,
+      loop: true
+    })
   }
 }
 </script>
@@ -35,11 +52,14 @@ export default {
   background-color: #3f3f3f !important;
   height: 100vh;
 }
-h2 {
-  color: white;
-}
+
+h2,
 h3 {
   color: white;
+  margin: 0; /* Remove any default margin */
+  position: relative; /* Position relative for absolute child elements */
+  overflow: hidden; /* Hide overflow text */
+  width: 100%; /* Ensure it takes the full width */
 }
 
 li {
@@ -53,17 +73,25 @@ li {
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  width: 100%;
 }
+
 .title {
-  margin-top: 40%;
+  margin-top: 20%;
   margin-bottom: 20px;
   font-weight: bold;
   font-size: 30px;
   text-align: center;
 }
+
 .subHeading {
   text-decoration: underline;
+  text-align: center;
 }
+.Typewriter__cursor {
+  position: fixed;
+}
+
 .itemsList {
   font-size: 15px;
   margin-bottom: 5px;
@@ -73,8 +101,9 @@ li {
   width: 300px;
   border-radius: 30px;
   display: none;
-  margin-top: 40%;
+  margin-top: 20%;
 }
+
 @media only screen and (min-width: 768px) {
   .firstImage {
     display: block;
@@ -82,13 +111,14 @@ li {
   .firstContainer {
     display: flex;
     flex-direction: row-reverse;
-    justify-content: space-evenly;
+    justify-content: space-around;
     align-items: center;
   }
 }
+
 @media only screen and (min-width: 1080px) {
   .firstImage {
-    width: 500px;
+    width: 600px;
   }
   .title {
     font-size: 40px;
@@ -96,28 +126,28 @@ li {
 }
 
 /* .top {
-  height: 100px;
-  width: 100%;
-} */
+    height: 100px;
+    width: 100%;
+  } */
 /* .bottom {
-  height: 200px;
-  width: 100%;
-}
-.triangle {
-  border-top: 250px solid #3f3f3f;
-  border-left: 3400px solid transparent;
-  border-right: 0px solid transparent;
-  bottom: -600px;
+    height: 200px;
+    width: 100%;
+  }
+  .triangle {
+    border-top: 250px solid #3f3f3f;
+    border-left: 3400px solid transparent;
+    border-right: 0px solid transparent;
+    bottom: -600px;
 
-  width: 0;
-  height: 0;
-  top: 107px;
-  content: '';
-  display: block;
-  position: absolute;
-  overflow: hidden;
-  left: 20;
-  right: 0;
-  margin: auto;
-} */
+    width: 0;
+    height: 0;
+    top: 107px;
+    content: '';
+    display: block;
+    position: absolute;
+    overflow: hidden;
+    left: 20;
+    right: 0;
+    margin: auto;
+  } */
 </style>
